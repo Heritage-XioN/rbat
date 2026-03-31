660fed5042ec87aa8c424dab5ce7e5201bacab3c
HERITAGE-XION <iheritage934@gmail.com>
2026-03-31 15:00:07 +0100

    added thsi error

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,13 @@
-#[derive(Debug)]
-pub enum Error {
-    Generic(String),
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum RbatError {
+    #[error("I/O error occurred")]
+    Io(#[from] std::io::Error),
+
+    #[error("Error occured while parsing binary")]
+    ParseError(#[from] goblin::error::Error),
+
+    #[error("error occured while disassembling binary bytes")]
+    DisassemblerError(#[from] capstone::Error),
 }
